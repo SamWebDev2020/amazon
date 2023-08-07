510916be53a9c2081eb8138692de394823bb28e6
Samuel Preetham <smlpreetham08@gmail.com>
2023-08-07 23:05:05 +0530

    Commiting test case for amazon

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -36,8 +36,24 @@ public class TestCases {
     public  void testCase01(){
         System.out.println("Start Test case: testCase01");
         driver.get("https://www.google.com");
-        System.out.println("end Test case: testCase02");
+        driver.findElement(By.id("APjFqb")).sendKeys("amazon");
+        driver.findElement(By.xpath("(//input[@value='Google Search'])[2]")).click();
+        Thread.sleep(3000);
+        List<WebElement> searchResults = driver.findElements(By.cssSelector("div.g"));
+            for (WebElement result : searchResults) {
+                // Check if the result contains "amazon.in" or "amazon.com"
+                if (result.getText().contains("amazon.in") || result.getText().contains("amazon.com")) {
+                    System.out.println("Search result contains 'amazon.in' or 'amazon.com'. Test passed.");
+                    break;
+                }
+            }else {
+                System.out.println("Search result does not contain 'amazon.in' or 'amazon.com'. Test failed.");
+            }
+    
+        System.out.println("end Test case: testCase01");
     }
+
+
 
 
 }
